42b0f74edc3b2d376f9f45cb2ec33a0c0a1b68cd
fayzvrow <faysalabdulkadir16@gmail.com>
2025-12-14 16:06:41 -0500

    Created storybook intro page

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>The Night Before Christmas</title>
+    <link rel="stylesheet" href="css/intro.css">
+</head>
+<body>
+    <div id="intro-scene">
+        <div id="book-container">
+            <div id="storybook">
+                <h1 class="book-title">The Night Before Christmas</h1>
+                <p class="book-subtitle">Click here to begin the story</p>
+                <img src="images/gift-sticker.png" class="sticker sticker-top-left">
+                <img src="images/reindeer-sticker.png" class="sticker sticker-bottom-right">
+                <img src="images/tree-sticker.png" class="sticker sticker-top-right">
+            </div>
+        </div>
+    </div>
+
+    <script src="js/intro.js"></script>
+</body>
+</html>
--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,9 @@
-intro.js
+const book = document.getElementById("storybook");
+
+book.addEventListener("click", () => {
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        window.location.href = "story.php";
+    }, 800);
+});
